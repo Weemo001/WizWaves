f6bbc3a3b3dd5eddc308990895fe4a68638d6d2a
Weemo <giantfrog137@gmail.com>
2024-12-08 20:01:03 -0700

    Shooting
    - Shooting implemented, enemies get cleared properly

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -6,10 +6,12 @@ extends CharacterBody3D
 
 @export var SPEED = 5.0
 @export var sensitivity = 0.5
+@export var shoot_interval = 0.3
 @export var health = 10
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	$ShootTimer.wait_time = shoot_interval
 
 func _input(event):
 	# Camera rotation
@@ -21,13 +23,19 @@ func _input(event):
 		# Prevent camera from flipping
 		camera_origin.rotation.x = clamp(camera_origin.rotation.x, deg_to_rad(-90), deg_to_rad(45))
 	
+	# Extra check on shooting to prevent an extra projectile from being shot when the player releases the shoot button
+	if event.is_action_released("shoot"):
+		if not $ShootTimer.is_stopped():
+			$ShootTimer.stop()
+	
 	# Quit game on button press
 	if event.is_action_pressed("quit"):
 		get_tree().quit()
 
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("shoot"):
-		shoot()
+	# When holding or pressing shoot button, shoot bullet at rate defined by shoot_interval
+	if Input.is_action_pressed("shoot") && $ShootTimer.is_stopped():
+		$ShootTimer.start()
 
 func _physics_process(_delta: float) -> void:
 	# Player movement 
@@ -45,11 +53,11 @@ func _physics_process(_delta: float) -> void:
 	move_and_slide()
 
 func shoot():
-	if bullet:
-		var bullet_instance = bullet.instantiate()
-		bullet_instance.global_position = $ProjectileSpawn.global_position
-		bullet_instance.direction = -global_transform.basis.z.normalized()
-		get_parent().add_child(bullet_instance)
+	# Create an instance of the bullet, set its position, direction, and spawn it
+	var bullet_instance = bullet.instantiate()
+	bullet_instance.global_position = $ProjectileSpawn.global_position
+	bullet_instance.direction = -global_transform.basis.z.normalized()
+	get_parent().add_child(bullet_instance)
 
 func take_damage(amount: float):
 	# Reduce health by amount, if health is 0, kill player
@@ -60,3 +68,6 @@ func take_damage(amount: float):
 		
 func _die():
 	print("Player has died!") # DEBUG
+
+func _on_shoot_timer_timeout() -> void:
+	shoot()
--- Scripts/Player/projectile.gd
+++ Scripts/Player/projectile.gd
@@ -7,6 +7,10 @@ func _ready() -> void:
 	gravity_scale = 0
 
 func _physics_process(delta: float) -> void:
+	# Set bullet movement, if it collides, check if it hit an enemy, if so clear enemy and remove from tracked array, then delete bullet, else just delete bullet
 	var collision = move_and_collide(direction * speed * delta)
 	if collision:
+		if collision.get_collider().is_in_group("enemy"):
+			collision.get_collider().queue_free()
+			Global.active_enemies.pop_back()
 		queue_free()
